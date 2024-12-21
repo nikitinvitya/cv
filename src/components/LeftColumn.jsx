b46333348b1b019cb9bc319706b634443d91bbb8
nikitinvitya <149611289+nikitinvitya@users.noreply.github.com>
2024-12-22 01:02:26 +0300

    Update LeftColumn.jsx

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,14 +1,13 @@
-// src/components/LeftColumn.jsx
 import React from 'react';
 import WorkExperience from './WorkExperience';
-import Projects from './Projects'; // Импортируем новый компонент
+import Projects from './Projects';
 import '../styles/LeftColumn.css';
 
 function LeftColumn() {
     return (
         <div className="left-column">
             <WorkExperience />
-            <Projects /> {/* Добавляем раздел с проектами */}
+            <Projects /> 
         </div>
     );
 }
